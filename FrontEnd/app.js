ff8fc5f22893d7721eaddf6c288b8dcf8ccfcd9d
Mathias <mathiasmaillydepinho@gmail.com>
2025-11-30 18:52:21 +0100

    commit N5

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,4 +1,4 @@
-//METHOD FETCH POUR APPELLER L'API
+//METHOD FETCH POUR APPELLER L'API TRAVAUX
 async function getWorks() {
   const url = "http://localhost:5678/api/works";
   try {
@@ -27,3 +27,34 @@ function setFigure (data) {
                         <figcaption>${data.title}</figcaption>`;
     document.querySelector(".gallery").append(figure);
 }
+
+//METHOD FETCH POUR APPELLER L'API CATEGORIES
+async function getCategories() {
+  const url = "http://localhost:5678/api/categories";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+
+    for (let i = 0; i <json.length; i++) {
+      setFilter(json[i]);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+// APPELLE DE LA FONCTION
+getCategories();
+
+//FUNCTION AJOUT DES CATEGORIES FILTRES DEPUIS JS
+function setFilter (data) {
+    const div = document.createElement("div");
+    div.innerHTML = `${data.name}`
+    document.querySelector(".filtres-container").append(div);
+}
+document.querySelector(".tous").addEventListener("click", () => getWorks)
+

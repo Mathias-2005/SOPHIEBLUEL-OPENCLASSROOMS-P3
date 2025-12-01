f0b193f764f523f4bf925be47bac424468e5c4da
Mathias <mathiasmaillydepinho@gmail.com>
2025-12-01 10:22:20 +0100

    ajout des filtres dynamique

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,31 +1,37 @@
 //METHOD FETCH POUR APPELLER L'API TRAVAUX
-async function getWorks() {
+async function getWorks(filter) {
+  document.querySelector(".gallery").innerHTML = "";
   const url = "http://localhost:5678/api/works";
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     const json = await response.json();
-    console.log(json);
-    // BOUCLE WORKS JSON AFFICHER DANS LA FONCTION 
-    for (let i = 0; i <json.length; i++) {
-      setFigure(json[i]);
+    if (filter) {
+      let filtered = json.filter ((data) => data.categoryId === filter);
+      for (let i = 0; i < filtered.length; i++) {
+        setFigure(filtered[i]);
+      }
+    }
+    else {
+      for (let i = 0; i < json.length; i++) {
+        setFigure(json[i]);
+      }
     }
   } catch (error) {
     console.error(error.message);
   }
 }
-// APPELLE DE LA FONCTION
+//APPELLE DE LA FONCTION
 getWorks();
 
-//FUNCTION AJOUT DES TRAVAUX DEPUIS JS
-function setFigure (data) {
-    const figure = document.createElement("figure");
-    figure.innerHTML = `<img src= ${data.imageUrl} alt= ${data.title}>
+//FUNCTION AJOUT DES TRAVAUX DEPUIS JS DYNAMIQUEMENT
+function setFigure(data) {
+  const figure = document.createElement("figure");
+  figure.innerHTML = `<img src= ${data.imageUrl} alt= ${data.title}>
                         <figcaption>${data.title}</figcaption>`;
-    document.querySelector(".gallery").append(figure);
+  document.querySelector(".gallery").append(figure);
 }
 
 //METHOD FETCH POUR APPELLER L'API CATEGORIES
@@ -40,7 +46,7 @@ async function getCategories() {
     const json = await response.json();
     console.log(json);
 
-    for (let i = 0; i <json.length; i++) {
+    for (let i = 0; i < json.length; i++) {
       setFilter(json[i]);
     }
   } catch (error) {
@@ -50,11 +56,13 @@ async function getCategories() {
 // APPELLE DE LA FONCTION
 getCategories();
 
-//FUNCTION AJOUT DES CATEGORIES FILTRES DEPUIS JS
-function setFilter (data) {
-    const div = document.createElement("div");
-    div.innerHTML = `${data.name}`
-    document.querySelector(".filtres-container").append(div);
+//FUNCTION AJOUT DES CATEGORIES FILTRES DEPUIS JS DYNAMIQUEMENT
+function setFilter(data) {
+  const div = document.createElement("div");
+  div.className = data.id;
+  div.addEventListener("click", () => getWorks(data.id));
+  div.innerHTML = `${data.name}`;
+  document.querySelector(".filtres-container").append(div);
 }
-document.querySelector(".tous").addEventListener("click", () => getWorks)
+document.querySelector(".tous").addEventListener("click", () => getWorks());
 

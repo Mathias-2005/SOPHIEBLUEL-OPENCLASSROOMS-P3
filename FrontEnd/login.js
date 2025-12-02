b042289d0d812b8e16c6ee0f18689cde4083dba2
Mathias <mathiasmaillydepinho@gmail.com>
2025-12-02 15:00:04 +0100

    ADD ERROR MESSAGE + LOGIN FINISH

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,16 +1,16 @@
 const url = "http://localhost:5678/api/users/login"; // URL DE L'API LOGIN
 
-document.getElementById("loginform").addEventListener("submit", getLogin);
+document.getElementById("loginform").addEventListener("submit", getLogin); // AU SUBMIT DU BOUTON ON LANCE LA FUNCTION GETLOGIN
 
-async function getLogin (event) {
-    event.preventDefault();  // ANNULE L'EVENT SI ANNULABLE
+async function getLogin(event) {
+    event.preventDefault();  // SI L'EVENT EST PAS GERER L'ACTION NE DEVRAIT PAS ETRE EXECUTER
 
     let user = {
-        email: document.getElementById("email").value, 
+        email: document.getElementById("email").value,
         password: document.getElementById("password").value
     };
 
-    let response = await fetch (url, {
+    let response = await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -18,7 +18,16 @@ async function getLogin (event) {
         body: JSON.stringify(user),
     });
 
-    let result = await response.json();
-    console.log(result);
-}
+    if (response.status != 200) {
+            const errorBox = document.getElementById("error"); // ON VIENS PRENDRE LA DIV ERROR DU HTML 
+            errorBox.style.display = "block"; // ON REMOVE LE DISPLAY NONE DU HTML
+            errorBox.innerText = "Erreur dans l’identifiant ou le mot de passe"; // ON LUI IMPLEMENTE LE TEXT 
+        } else {
+            let result = await response.json();
+            const token = result.token; // CREATION VARIABLE TOKEN 
+            sessionStorage.setItem("authToken", token);
+            window.location.href = "index.html"; // REDIRIGE VERS LA PAGE D'ACCEUIL SI LOGIN AUTORISER
+        }
+    }
+
 //getLogin();
